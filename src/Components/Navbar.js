a6795c0fe1e962e479c003d66fbbe2ac5d9b481a
Arav1T <aravtiwari127@gmail.com>
2023-07-07 14:23:26 +0530

    Border_Change

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,25 +1,25 @@
-import React, { useRef } from 'react'
+import React  from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Navbar() {
-    const [ChangeColour,ChangeColour2,ChangeColour1,ChangeColour3,ChangeColour4,ChangeColour5] = [useRef(null),useRef(null),useRef(null),useRef(null),useRef(null),useRef(null)]
+    
     return (
       <div>
         <nav className='flex justify-between px-8 bg-slate-600 text-white p-5'>
             <div >
             <ul className='flex space-x-4'>
-                <li ref={ChangeColour}><button onClick={()=>[ChangeColour.current.style.borderBottom,ChangeColour1.current.style.borderBottom,ChangeColour2.current.style.borderBottom,ChangeColour3.current.style.borderBottom,ChangeColour4.current.style.borderBottom,ChangeColour5.current.style.borderBottom]=["2px solid black",null,null,null,null,null]}><Link to="/general">General</Link></button>  </li>
-                <li ref={ChangeColour1}> <button onClick={()=>[ChangeColour1.current.style.borderBottom,ChangeColour.current.style.borderBottom,ChangeColour2.current.style.borderBottom,ChangeColour3.current.style.borderBottom,ChangeColour4.current.style.borderBottom,ChangeColour5.current.style.borderBottom]=["2px solid black",null,null,null,null,null]}><Link to="/business">Business</Link></button> </li>
-                <li ref={ChangeColour2}> <button onClick={()=>[ChangeColour2.current.style.borderBottom,ChangeColour1.current.style.borderBottom,ChangeColour.current.style.borderBottom,ChangeColour3.current.style.borderBottom,ChangeColour4.current.style.borderBottom,ChangeColour5.current.style.borderBottom]=["2px solid black",null,null,null,null,null]}><Link to="/entertainment">Entertaiment</Link></button> </li>
+                <li  className="hover:border-b-2 hover:border-black" ><button ><Link to="/general">General</Link></button>  </li>
+                <li r className="hover:border-b-2 hover:border-black" > <button ><Link to="/business">Business</Link></button> </li>
+                <li r className="hover:border-b-2 hover:border-black" > <button ><Link to="/entertainment">Entertaiment</Link></button> </li>
                 
-                <li ref={ChangeColour3}> <button onClick={()=>[ChangeColour3.current.style.borderBottom,ChangeColour1.current.style.borderBottom,ChangeColour2.current.style.borderBottom,ChangeColour.current.style.borderBottom,ChangeColour4.current.style.borderBottom,ChangeColour5.current.style.borderBottom]=["2px solid black",null,null,null,null,null]}>
+                <li r className="hover:border-b-2 hover:border-black" > <button >
                 <Link to="/technology">Technology</Link>
                 </button> </li>
-                <li ref={ChangeColour4}>
-                    <button onClick={()=>[ChangeColour4.current.style.borderBottom,ChangeColour1.current.style.borderBottom,ChangeColour2.current.style.borderBottom,ChangeColour3.current.style.borderBottom,ChangeColour.current.style.borderBottom,ChangeColour5.current.style.borderBottom]=["2px solid black",null,null,null,null,null]}><Link to="/custom">Custom</Link></button>
+                <li r className="hover:border-b-2 hover:border-black" >
+                    <button ><Link to="/custom">Custom</Link></button>
                 </li>
-                <li ref={ChangeColour5}>
-                    <button onClick={()=>[ChangeColour5.current.style.borderBottom,ChangeColour1.current.style.borderBottom,ChangeColour2.current.style.borderBottom,ChangeColour3.current.style.borderBottom,ChangeColour.current.style.borderBottom]=["2px solid black",null,null,null,null]}><Link to="/Todo">ToDo</Link></button>
+                <li r className="hover:border-b-2 hover:border-black" >
+                    <button ><Link to="/Todo">ToDo</Link></button>
                 </li>
                
             </ul> 
